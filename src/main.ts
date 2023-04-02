5bc02ac7831de08a9bed3787e9657e3550eef190
fickler <2586844575@qq.com>
2023-04-03 00:12:21 +0800

    second

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import {Button, Icon, NavBar, Tabbar, TabbarItem } from 'vant'
-import App from './App.vue'
+import { createApp } from 'vue';
+import {Button, Icon, NavBar, Tabbar, TabbarItem } from 'vant';
+import App from './App.vue';
 import 'vant/es/toast/style';
+import 'vant/lib/index.css';
+import * as VueRouter from "vue-router";
+import routes from "./config/route";
 
 const app = createApp(App);
 app.use(Button);
@@ -9,4 +12,13 @@ app.use(Icon);
 app.use(NavBar);
 app.use(Tabbar)
 app.use(TabbarItem)
+
+const router = VueRouter.createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: VueRouter.createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+})
+
+app.use(router);
+
 app.mount('#app');
---
+++ src/pages/UserPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <van-cell title="昵称" is-link to="/user/edit" :value="user.username" />
+  <van-cell title="账号" :value="user.userAccount" />
+  <van-cell title="头像" is-link to="/user/edit">
+    <img style="height: 48px" :src="user.avatarUrl" />
+  </van-cell>
+  <van-cell title="性别" is-link to="/user/edit" :value="user.gender" @click="toEdit('gender', '性别', user.gender)" />
+  <van-cell title="电话" is-link to="/user/edit" :value="user.phone" @click="toEdit('phone', '电话', user.phone)" />
+  <van-cell title="邮箱" is-link to="/user/edit" :value="user.email" />
+  <van-cell title="星球编号" :value="user.planetCode" />
+  <van-cell title="注册时间" :value="user.createTime.toISOString()" />
+</template>
+
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+
+const user = {
+  id: 1,
+  username: 'fickle',
+  userAccount: 'fickle',
+  avatarUrl: 'http://qzapp.qlogo.cn/qzapp/101983660/DE2DFA542010DAD2696FC2082D1EE3E7/100',
+  gender: '男',
+  phone: '123',
+  email: '456@qq.com',
+  planetCode: '1234',
+  createTime: new Date(),
+}
+const router = useRouter();
+const toEdit = (editKey: string, editName: string, currentValue: string) => {
+  router.push({
+    path: '/user/edit',
+    query: {
+      editKey,
+      editName,
+      currentValue,
+    }
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
