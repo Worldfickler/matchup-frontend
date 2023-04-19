2e80385eaaff6c42e158174b71b88ded5e372a5e
fickler <2586844575@qq.com>
2023-04-19 23:22:08 +0800

    搜索队伍

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="teamPage">
+    <van-search v-model="searchText" placeholder="搜索队伍" @search="onSearch" />
     <van-button type="primary" @click="doJoinTeam">加入队伍</van-button>
     <team-card-list :team-list="teamList" />
+    <van-empty v-if="teamList?.length < 1" description="结果为空" />
   </div>
 </template>
 
@@ -14,6 +16,7 @@ import myAxios from "../plugins/myAxios.ts";
 import {Toast} from "vant";
 
 const router = useRouter()
+const searchText = ref('')
 
 const doJoinTeam = () => {
   router.push({
@@ -23,15 +26,28 @@ const doJoinTeam = () => {
 
 const teamList = ref([]);
 
-// 在页面加载时只触发一下
-onMounted(async () => {
-  const res = await myAxios.get("/team/list");
+const listTeam = async (val = '') => {
+  const res = await myAxios.get("/team/list", {
+    params: {
+      searchText: val,
+      pageNum: 1,
+    }
+  });
   if (res?.code === 0) {
     teamList.value = res.data;
   } else {
     Toast.fail("加载队伍失败，请刷新重试")
   }
+}
+
+// 在页面加载时只触发一下
+onMounted(  () => {
+  listTeam();
 })
+
+const onSearch = (val) => {
+  listTeam(val);
+};
 
 </script>
 
