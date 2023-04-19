8040c783b87b2c07e6756776d3f342a9f135d468
fickler <2586844575@qq.com>
2023-04-19 11:59:41 +0800

    队伍表单

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="teamPage">
     <van-button type="primary" @click="doJoinTeam">加入队伍</van-button>
+    <team-card-list :team-list="teamList" />
   </div>
 </template>
 
 <script setup>
 
 import {useRouter} from "vue-router";
+import TeamCardList from "../components/TeamCardList.vue";
+import {onMounted, ref} from "vue";
+import myAxios from "../plugins/myAxios.ts";
+import {Toast} from "vant";
 
 const router = useRouter()
 
@@ -15,6 +20,18 @@ const doJoinTeam = () => {
     path: "/team/add"
   })
 }
+
+const teamList = ref([]);
+
+// 在页面加载时只触发一下
+onMounted(async () => {
+  const res = await myAxios.get("/team/list");
+  if (res?.code === 0) {
+    teamList.value = res.data;
+  } else {
+    Toast.fail("加载队伍失败，请刷新重试")
+  }
+})
 
 </script>
 
--- src/service/user.ts
+++ src/service/user.ts
@@ -8,6 +8,7 @@ export const getCurrentUser = async () => {
     }
     // 不存在则从远程获取
     const res = await myAxios.get('/user/current');
+    //@ts-ignore
     if (res.code === 0) {
         setCurrentUserState(res.data);
         return res.data;
